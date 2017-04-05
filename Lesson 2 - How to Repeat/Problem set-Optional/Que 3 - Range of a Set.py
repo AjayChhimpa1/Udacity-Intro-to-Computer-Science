fe21d86a9683fca233c91c4c5578474f2a4e9a59
Ajay Kumar Chhimpa <ajaychhimpa1@gmail.com>
2017-04-06 00:10:48 +0530

    Que 3 - Range of a Set.py

---
+++ Lesson 2 - How to Repeat/Problem set-Optional/Que 3 - Range of a Set.py
@@ -0,0 +1,23 @@
+# The range of a set of values is the maximum value minus the minimum
+# value. Define a procedure, set_range, which returns the range of three input
+# values.
+
+# Hint: the procedure, biggest which you coded in this unit
+# might help you with this question. You might also like to find a way to
+# code it using some built-in functions.
+
+def set_range(a,b,c):
+    big = max(a,b,c)
+    if big == a:
+        return a - min(b,c)
+    elif big == b:
+        return b - min(a,c)
+    else:
+        return c - min(a,c)
+
+
+print set_range(10, 4, 7)
+#>>> 6  # since 10 - 4 = 6
+
+print set_range(1.1, 7.4, 18.7)
+#>>> 17.6 # since 18.7 - 1.1 = 17.6
