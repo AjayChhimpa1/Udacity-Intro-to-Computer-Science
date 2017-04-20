d72865f6ddeb592afe67ffd34cc9dea59db3323d
Ajay Kumar Chhimpa <ajaychhimpa1@gmail.com>
2017-04-20 17:55:16 +0530

    Que 3 - Converting Seconds.py

---
+++ Lesson 4 - Responding to Queries/Problem Set - Optional/Que 3 - Converting Seconds.py
@@ -0,0 +1,41 @@
+# Write a procedure, convert_seconds, which takes as input a non-negative 
+# number of seconds and returns a string of the form 
+# '<integer> hours, <integer> minutes, <number> seconds' but
+# where if <integer> is 1 for the number of hours or minutes, 
+# then it should be hour/minute. Further, <number> may be an integer
+# or decimal, and if it is 1, then it should be followed by second.
+# You might need to use int() to turn a decimal into a float depending
+# on how you code this. int(3.0) gives 3
+#
+# Note that English uses the plural when talking about 0 items, so
+# it should be "0 minutes".
+#
+
+def convert_seconds(seconds):
+    hours = int(seconds / 3600)
+    
+    mins = int((seconds - (hours * 3600)) / 60)
+    
+    secs = seconds - ((hours * 3600) + (mins * 60))
+    
+    if hours != 1 and mins != 1 and secs != 1:
+        return str(hours)+' hours, ' + str(mins)+' minutes, ' + str(secs)+' seconds'
+    elif hours != 1 and mins != 1:
+        return str(hours)+' hours, ' + str(mins)+' minutes, ' + str(secs)+' second'
+    elif hours != 1 and secs != 1:
+        return str(hours)+' hours, ' + str(mins)+' minute, ' + str(secs)+' seconds'
+    elif mins != 1 and secs != 1:
+        return str(hours)+' hour, ' + str(mins)+' minutes, ' + str(secs)+' seconds'
+    else:
+        return str(hours)+' hour, ' + str(mins)+' minute, ' + str(secs)+' second'
+    
+
+
+print convert_seconds(3661)
+#>>> 1 hour, 1 minute, 1 second
+
+print convert_seconds(7325)
+#>>> 2 hours, 2 minutes, 5 seconds
+
+print convert_seconds(7261.7)
+#>>> 2 hours, 1 minute, 1.7 seconds
